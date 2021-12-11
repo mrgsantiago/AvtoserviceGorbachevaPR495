e8a099841d0a04651ebb359487b4c0dd62ef0fc2
mrgsantiago <71941008+mrgsantiago@users.noreply.github.com>
2021-12-11 16:52:34 +0700

    Add files via upload

---
+++ avtoserviceGorbachevaMN/avtoserviceGorbachevaMN/MainW.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace avtoserviceGorbachevaMN
+{
+    /// <summary>
+    /// Логика взаимодействия для MainW.xaml
+    /// </summary>
+    public partial class MainW : Window
+    {
+        public MainW()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new PageMain());
+        }
+        private void BtnBak_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page1());
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page2());
+        }
+    }
+}
